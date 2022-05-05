8b8e8a1ff72a4770deadd744490cb552ca5c0b0c
didi <victoria.chalai@epfl.ch>
2022-05-05 23:05:19 +0200

    blinky LED

--- alert_LEDs.c
+++ alert_LEDs.c
@@ -1,8 +1,16 @@
-/*
- * alert_LEDs.c
- *
- *  Created on: 5 mai 2022
- *      Author: victo
- */
+#include <system_clock_config.h>
+#include <gpio.h>
+#include <main.h>
+#include <timer.h>
+#include <motor.h>
+#include <selector.h>
 
 
+void blinky_led(){
+		gpio_config_output_pushpull(FRONT_LED);
+
+		while (1) {
+		delay(SystemCoreClock/16);
+		gpio_toggle(FRONT_LED);
+		}
+}
